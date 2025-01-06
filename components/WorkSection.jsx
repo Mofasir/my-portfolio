e4475f4e4a9397b4204c96da760e1623ca03a2a6
Mohammad Faikar Natsir <misteralfikri@gmail.com>
2025-01-06 15:45:42 +0700

    add blank target to live and github link

--- components/WorkSection.jsx
+++ components/WorkSection.jsx
@@ -135,7 +135,7 @@ const WorkSection = () => {
                             {/* buttons */}
                             <div className="flex items-center gap-4">
                                 {/* live project button */}
-                                <Link href={project.live}>
+                                <Link href={project.live} target="_blank">
                                     <TooltipProvider delayDuration={100}>
                                         <Tooltip>
                                             <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
@@ -148,7 +148,7 @@ const WorkSection = () => {
                                     </TooltipProvider>
                                 </Link>
                                 {/* github project button */}
-                                <Link href={project.github}>
+                                <Link href={project.github} target="_blank">
                                     <TooltipProvider delayDuration={100}>
                                         <Tooltip>
                                             <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
